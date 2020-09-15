b5c2b89c4d83e7f3d8832da9a001ac49b221e702
flys345 <70917062+flys345@users.noreply.github.com>
2020-09-15 09:40:46 -0500

    Add files via upload

---
+++ FlysAndroid-VPS-MX.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/flys345/ADM-VPS-MX-MOD-FLYS_ANDROID/master/instalscript.sh; chmod 777 instalscript.sh; ./instalscript.sh
